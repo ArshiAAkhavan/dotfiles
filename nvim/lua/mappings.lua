c7de87a1a61a1bc195126c928b106a4bdfabb8b3
Ark <ark@sotoon.ir>
2024-07-19 16:24:08 +0330

    fix mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "Code action" })
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
