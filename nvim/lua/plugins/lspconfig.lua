f1a0c9b6aad0b0aa257b83b8878b784963165fb7
Ark <ark@sotoon.ir>
2024-07-20 20:06:21 +0330

    proper mapping structure for lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,5 +4,8 @@ local M = {
     require("nvchad.configs.lspconfig").defaults()
     require "configs.lspconfig"
   end,
+  mapping = function()
+    vim.keymap.set({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "Code action" })
+  end,
 }
 return M
