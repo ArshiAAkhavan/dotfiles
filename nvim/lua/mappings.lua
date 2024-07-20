9c73d887f88906fe1cda97e4407dc0746415891d
Ark <ark@sotoon.ir>
2024-07-20 20:11:57 +0330

    proper mapping structure for lspconfig

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,7 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "Code action" })
 
 -- save on ctrl+S
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
