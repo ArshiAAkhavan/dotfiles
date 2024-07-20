2be772ede26e827a2b45d447922d32fe68161b44
Ark <ark@sotoon.ir>
2024-07-20 20:02:16 +0330

    ctrl+s and ctrl+a

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,7 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "Code action" })
+
+-- save on ctrl+S
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- coppy all on ctrl+A
+map({ "n", "i", "v" }, "<C-a>", "<cmd> %y+ <CR>")
 
 require("plugins.neotest").mapping()
 require("plugins.dap").mapping()
