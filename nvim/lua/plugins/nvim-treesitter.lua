ebed14674af1ad5eda0e04863d8d1dc33212100f
Ark <ark@sotoon.ir>
2024-07-21 10:26:11 +0330

    fix colors in nvim-treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -18,5 +18,27 @@ local M = {
       "bash",
     },
   },
+  config = function(_, opts)
+    -- punctuation colors
+    vim.api.nvim_set_hl(0, "@punctuation", { fg = "#e67e80", bg = "", italic = false, underline = false, sp = "" })
+    vim.api.nvim_set_hl(
+      0,
+      "@punctuation.delimiter",
+      { fg = "#e67e80", bg = "", italic = false, underline = false, sp = "" }
+    )
+    vim.api.nvim_set_hl(
+      0,
+      "@punctuation.bracket",
+      { fg = "#e67e80", bg = "", italic = false, underline = false, sp = "" }
+    )
+
+    -- documentation colors
+    vim.api.nvim_set_hl(
+      0,
+      "@comment.documentation",
+      { fg = "#d08e6e", bg = "", italic = false, underline = false, sp = "" }
+    )
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
 return M
