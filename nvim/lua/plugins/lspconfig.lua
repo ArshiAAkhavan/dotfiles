702c0266bb0b3c15c66860feb374400a7bb25ec2
Ark <ark@sotoon.ir>
2024-07-25 09:58:47 +0330

    kemap for toggle inlay_hints

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,12 @@ local M = {
   end,
   mapping = function()
     vim.keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, { desc = "Code action" })
+    vim.keymap.set({ "n", "v" }, "gt", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, { desc = "  toggle inlay_hint" })
+    vim.keymap.set({ "n", "v" }, "gh", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, { desc = "  toggle inlay_hint" })
   end,
 }
 return M
