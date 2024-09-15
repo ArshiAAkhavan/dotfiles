a505f728f7b4d186b6140dca325861bdccac368d
Ark <ark@sotoon.ir>
2024-09-15 17:55:37 +0330

    relative number by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,6 +4,7 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
+o.relativenumber = true
 
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
