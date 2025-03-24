d1f41eaa73450de2cbc53b86a9f522cda9623ccc
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-03-24 12:13:53 -0400

    set max_lines form NVTS_context

--- nvim/lua/plugins/nvim-tree-sitter-context.lua
+++ nvim/lua/plugins/nvim-tree-sitter-context.lua
@@ -1,5 +1,19 @@
 local M = {
   "nvim-treesitter/nvim-treesitter-context",
   event = "BufEnter",
+  config = function()
+    require("treesitter-context").setup {
+      enable = true,
+      max_lines = 3,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = "outer",
+      mode = "cursor",
+      zindex = 20,
+      on_attach = nil,
+      win_opts = {},
+    }
+  end,
 }
 return M
