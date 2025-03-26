00cc44e337ae9280f0348e5c1d611d030535a37f
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-03-26 16:01:09 -0400

    Add default LSP configs tu rustacean

--- nvim/lua/plugins/rustacean.lua
+++ nvim/lua/plugins/rustacean.lua
@@ -1,7 +1,20 @@
 local M = {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  lazy = false,   -- This plugin is already lazy
+  config = function()
+    require("nvchad.configs.lspconfig").defaults()
+    local nvlsp = require "nvchad.configs.lspconfig"
+
+    vim.g.rustaceanvim = {
+      -- LSP configuration
+      server = {
+        on_attach = nvlsp.on_attach,
+        on_init = nvlsp.on_init,
+        capabilities = nvlsp.capabilities,
+      },
+    }
+  end
 }
 
 return M
