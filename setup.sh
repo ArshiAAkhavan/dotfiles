4301f923e687a40cd8c29453b2e2279dc8bbea8f
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-06-03 19:22:18 -0400

    remove icons

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function link_dotfiles {
+  mkdir -p ~/$1 
+  src=$1
+  for file in $(/usr/bin/ls $src)
+  do
+    dst=$src/$file
+  	echo $dst
+    if [ -e ~/$dst ]; then
+  	  unlink ~/$dst
+   	fi
+  	ln -s $PWD/$dst ~/$dst
+  done
+}
+
+link_dotfiles .config
+link_dotfiles .local/share
+link_dotfiles .themes
+link_dotfiles .icons
