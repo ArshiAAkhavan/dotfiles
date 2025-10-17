768b56b736026cba4a0bfbbc2b412bb2a5088db7
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-10-17 15:36:25 -0700

    depricate nvim-lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,11 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local nvlsp = require "nvchad.configs.lspconfig"
-
 local servers = {
   -- lua
   "lua_ls",
@@ -31,12 +26,5 @@ local servers = {
 }
 
 vim.lsp.inlay_hint.enable(true)
+vim.lsp.enable(servers)
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
